d53297d13f8d3164fd9f21f6f2b382c04dafeae6
Smail Happy <Smailerz@yandex.ru>
2025-01-31 15:46:33 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -8,12 +8,13 @@ while True:
     if guess < number :
         print('Ваше число меньше того, что загадано.')
     
-    if guess > number :
+    elif guess > number :
         print('Ваше число больше того, что загадано.')
 
-    if guess == number :
+    elif guess == number :
         break
     count += 1
+    
 
 print('Отличная интуиция! Вы угадали число :0')
 print(f'У вас ушло на это {count} попыток.')
